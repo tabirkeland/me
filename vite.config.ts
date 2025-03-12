12eb2c1b189bb8c1a2cd97b9a3f35f07b212762e
Taylor <tabirkeland@gmail.com>
2025-03-12 17:00:40 -0500

    more deployment work

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Replace 'portfolio' with your repository name
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
